18e526b0fcabde4521505b18bfd36f32443ef9f5
Brandon Montalvo <79609464+brand0nM@users.noreply.github.com>
2022-06-19 17:32:58 -0600

    hw item 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,13 @@
 library(tidyverse)
 mechaCarMpg <- read.csv('MechaCar_mpg.csv')
+suspensionCoil <- read.csv('Suspension_Coil.csv')
+suspensionCoil$Manufacturing_Lot <- factor(suspensionCoil$Manufacturing_Lot)
+
 mpgLinReg <- lm(mpg~AWD+ground_clearance+spoiler_angle+vehicle_weight+vehicle_length,mechacar_mpg)
 summary(mpgLinReg)
+
+total_summary <- data.frame(mean(suspensionCoil$PSI),median(suspensionCoil$PSI),
+                            var(suspensionCoil$PSI),sd(suspensionCoil$PSI))
+
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% 
+  summarize(c(mean(PSI)),c(median(PSI)),c(var(PSI)),c(sd(PSI)))
